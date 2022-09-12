b22f1dda5b774cd2d456de5d03d506aed54aab85
Nursena Kurubas <nursena.krbs@gmail.com>
2022-09-12 02:02:20 -0400

    yaml files converted to text

--- convert2pdf.py
+++ convert2pdf.py
@@ -15,15 +15,20 @@ for root, dirs, files in os.walk(os.getcwd()):
         for file in files:
             path_no_ext = os.path.splitext(os.path.join(root,file))[0] 
             print(file)
-            if file.endswith(".md") or file.endswith(".rst") or file.endswith(".html"):
+            if file.endswith(".md") or file.endswith(".rst") or file.endswith(".html") or file.endswith(".yml"):
                 try:
                     if file.endswith(".md"):
                         os.system('md-to-pdf ' + path_no_ext + ".md")
                         os.remove(path_no_ext + ".md")
-                    if file.endswith(".rst"):
+                    elif file.endswith(".rst"):
                         os.system("rst2html5.py "+ path_no_ext+".rst " + path_no_ext + ".html" )
                         os.remove(path_no_ext + ".rst")
                         os.system("wkhtmltopdf "+ path_no_ext + ".html " + path_no_ext+".pdf")
                         os.remove(path_no_ext + ".html")
+                    elif file.endswith(".html"):
+                        os.system("wkhtmltopdf "+ path_no_ext + ".html " + path_no_ext+".pdf")
+                        os.remove(path_no_ext + ".html")
+                    elif file.endswith(".yml"):
+                        os.rename(path_no_ext + ".yml", path_no_ext + ".txt")
                 except WindowsError:
                     continue
